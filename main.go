6d38ef1ffd44f07e352fe6e6d849ab66cc4221ca
Faldy Findraddy <faldy.findraddy@tiket.com>
2020-06-22 10:01:22 +0700

    initial

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-redis/redis"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var ctx = context.Background()
+var httpStatus = ""
+var arguments = os.Args
+
+func rClient() *redis.Client {
+	redisAddr := arguments[1]
+	redisPassword := arguments[2]
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+	})
+
+	return client
+}
+
+func ping(client *redis.Client) error {
+	pong, err := client.Ping(ctx).Result()
+	if err != nil {
+		return err
+	}
+	fmt.Println(pong, err)
+
+	return nil
+}
+
+func healthcheck(c echo.Context) error {
+	// creates a client
+	client := rClient()
+
+	// check connection status
+	err := ping(client)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Redis ERROR")
+	}
+	return c.String(http.StatusOK, "Redis OK")
+}
+
+func main() {
+	if len(arguments) == 2 {
+		fmt.Println("Please provide host:port <password>.")
+		os.Exit(1)
+	}
+
+	// Echo instance
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
+
+	// Route => handler
+	e.GET("/healthcheck", healthcheck)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
